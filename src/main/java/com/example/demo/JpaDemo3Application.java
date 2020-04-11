00f043cbecc3122849fce3ac92f78fd0c6b0594b
Sudhir Atkire <vertical.sudhir>
2020-04-11 11:57:33 +0530

    initial commit

---
+++ src/main/java/com/example/demo/JpaDemo3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDemo3Application.class, args);
+	}
+
+}
